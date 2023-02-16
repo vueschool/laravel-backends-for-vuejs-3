f01baf9b0727dbcb988c26077d5adf92b11b1840
Daniel Kelly <me@danielkelly.io>
2023-02-16 09:51:49 -0600

    11-format-the-user-data-once

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,10 +1,36 @@
 import axios from "axios";
 export const useAuth = () => {
-  // user
-  // email -> string
-  // name -> string
-  // created_at -> Date
-  // updated_at -> Date
+  interface User {
+    email: string;
+    email_verified_at?: Date;
+    id: number;
+    name: string;
+    two_factor_confirmed_at?: Date;
+    two_factor_recovery_codes?: number;
+    two_factor_secret?: string;
+    updated_at: Date;
+    created_at: Date;
+  }
+  async function getUser(): Promise<User | null> {
+    try {
+      const res = await axios.get("/user");
+      const user = res.data;
+      return {
+        ...user,
+        created_at: new Date(user.created_at),
+        updated_at: new Date(user.updated_at),
+        email_verified_at: user.email_verified_at
+          ? new Date(user.email_verified_at)
+          : null,
+        two_factor_confirmed_at: user.two_factor_confirmed_at
+          ? new Date(user.two_factor_confirmed_at)
+          : null,
+      };
+    } catch (err) {
+      return null;
+    }
+  }
+
   // login
   interface LoginPayload {
     email: string;
