3150aa849b56ccdfceae8b81e09ec20ccc990c4c
Daniel Kelly <me@danielkelly.io>
2023-02-17 16:51:51 -0600

    15-manually-handle-form-validation-errors-with-laravel-and-vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { AxiosError } from "axios";
+
 definePageMeta({
   layout: "centered",
   middleware: ["guest"],
@@ -10,19 +12,40 @@ const form = ref({
   email: "",
   password: "",
 });
+
+const errors = ref({
+  email: [],
+  password: [],
+});
+
+async function handleLogin() {
+  try {
+    await login(form.value);
+  } catch (err) {
+    if (err instanceof AxiosError && err.response?.status === 422) {
+      errors.value = err.response.data.errors;
+    }
+  }
+}
 </script>
 <template>
   <div class="login">
     <h1>Login</h1>
-    <form @submit.prevent="login(form)">
+    <form @submit.prevent="handleLogin">
       <label>
         <div>Email</div>
         <input type="text" v-model="form.email" />
+        <div class="text-red-600 p-1" v-for="error in errors.email">
+          {{ error }}
+        </div>
       </label>
 
       <label>
         <div>Password</div>
         <input type="password" v-model="form.password" />
+        <div class="text-red-600 p-1" v-for="error in errors.password">
+          {{ error }}
+        </div>
       </label>
       <button class="btn">Login</button>
     </form>
