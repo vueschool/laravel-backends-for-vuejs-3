8bbd4dd12a9d637211f0b25fcb5f4b7761bfd33c
Daniel Kelly <me@danielkelly.io>
2023-03-21 11:15:04 -0500

    29-update-a-resource-with-vue-and-laravel

--- pages/links/[id].vue
+++ pages/links/[id].vue
@@ -1,14 +1,37 @@
-<script setup>
+<script setup lang="ts">
+import type { Link } from "@/types";
+import { FormKitNode } from "@formkit/core";
 definePageMeta({
   middleware: ["auth"],
 });
+
+const { find, link, update } = useLinks();
+await find(Number(useRoute().params.id));
+
+async function handleUpdate(payload: Partial<Link>, node?: FormKitNode) {
+  try {
+    await update(Number(useRoute().params.id), payload);
+  } catch (err) {
+    handleInvalidForm(err, node);
+  }
+}
 </script>
 <template>
   <h1>Update Link</h1>
   <GoBack>or go back to links</GoBack>
 
-  <FormKit type="form" submit-label="Update Link">
-    <FormKit type="text" name="short_link" label="Short Link" />
-    <FormKit type="url" name="full_link" label="Full Link" />
+  <FormKit type="form" submit-label="Update Link" @submit="handleUpdate">
+    <FormKit
+      :value="link.short_link"
+      type="text"
+      name="short_link"
+      label="Short Link"
+    />
+    <FormKit
+      :value="link.full_link"
+      type="url"
+      name="full_link"
+      label="Full Link"
+    />
   </FormKit>
 </template>
